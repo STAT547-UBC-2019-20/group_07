a98e96c8e80713b0d4ff6fa9af4e5b9d9f355211
lucymosquera <bellemarelucy@gmail.com>
2020-03-17 16:05:13 -0700

    Knit report to docs/specified folder

---
+++ Makefile
@@ -0,0 +1,6 @@
+# author: Lucy Mosquera & Denitsa Vasileva
+# date: March 17th, 2020
+
+.PHONY: all clean
+
+all: docs/finalreport.html docs/finalreport.pdf
--- scripts/knit.R
+++ scripts/knit.R
@@ -1,27 +1,33 @@
 "Script to knit the final report.
-Usage: knit.R --final_report=<report>" -> doc
+Usage: knit.R --final_report=<report> --report_dir=<report_dir>" -> doc
 
 # load packages
 suppressMessages(library(docopt))
 suppressMessages(library(rmarkdown))
 suppressMessages(library(glue))
 suppressMessages(library(bookdown))
+suppressMessages(library(here))
 
 opt <- docopt(doc)
 
 #' @author Denitsa Vasileva & Lucy Mosquera
 #' @param report path to the final report
 
-knit_report <- function(report){
+knit_report <- function(report, report_dir){
   # check that the report file we want to knit exists 
   if(!file.exists(report)) {
     stop(glue("Report file {report} not found"))
   }
+  # check that the output location exists
+  if (! dir.exists(here(report_dir))){
+    stop(glue("Report output directory: {report_dir} not found"))
+  }
   # knit report file
-  rmarkdown::render(report, c("bookdown::pdf_document2", "bookdown::html_document2"))
+  rmarkdown::render(report, c("bookdown::pdf_document2", "bookdown::html_document2"), 
+                    output_dir = here(report_dir))
   # print completion statement
   print("Rmarkdown file knit successfully!")
 }
 
 
-knit_report(opt$final_report)
+knit_report(report = opt$final_report, report_dir = opt$report_dir)
