891ec060c60fc5f3a25bba1e04ccd72b8d963bb4
lucymosquera <bellemarelucy@gmail.com>
2020-03-17 14:23:36 -0700

    Update lin reg w docopt

--- scripts/linear_regression.R
+++ scripts/linear_regression.R
@@ -13,28 +13,56 @@ suppressMessages(library(broom))
 suppressMessages(library(here))
 
 "This script performs linear regression
-Usage: linear_regression.R --clean_data_path=<clean_data_path> --output_path=<output_path>
+Usage: linear_regression.R --clean_data_path=<clean_data_path> --output_path_image=<output_path_image> --output_path_model=<output_path_model>
 "-> doc
 
-linear_regression<- function(){
+# Loading command line arguments 
+opt <- docopt(doc)
+
+linear_regression<- function(clean_data_path, output_path_image, output_path_model){
+  runModel <- TRUE
   # Read-in cleaned data file
   if (file.exists(here(clean_data_path))){
     data <- read.csv(here(clean_data_path))
-  } else{
+  } 
+  else{
     print(glue("Invalid clean data path: {clean_data_path}"))
-  } else if (!dir.exists(here(output_path))){ # check if output_path exists 
-    print(glue("Invalid image directory path: {output_path}"))
-  } else{
+    runModel <- FALSE
+  } 
+  if (! dir.exists(here(output_path_model))){ 
+    # check if output_path_model exists 
+    print(glue("Invalid model directory path: {output_path_model}"))
+    runModel <- FALSE
+  } 
+  if (! dir.exists(here(output_path_image))){ 
+    # check if output_path_image exists 
+    print(glue("Invalid image directory path: {output_path_image}"))
+    runModel <- FALSE
+  } 
+  if (runModel){
+    ## Linear regression of polarity (positivity of the work) vs. publication year
     model<- lm(data$polarity ~ data$publication.year)
-  data %>%
-    ggplot(aes(x= publication.year,y=polarity))+geom_smooth(method = "lm")+  theme_bw() + geom_point()+
-    xlab("Publication Year") +
-    ylab("Polarity") 
-    ggsave(file= here("images","year_polarity.pdf"))
-    saveRDS(tidy(model), file = here("data","tidy_model.rds"))
-    saveRDS(augment(model), file=here("data","augment_model.rds") )
-    saveRDS(glance(model), file=here("data","glance_model.rds") )
+    
+    ## Plot of the regression line
+    data %>%
+      ggplot(aes(x = publication.year, y = polarity)) + 
+        geom_smooth(method = "lm") +
+        theme_bw() + 
+        geom_point() +
+        xlab("Publication Year") +
+        ylab("Polarity") 
+    ggsave(file= here(output_path_image,"year_polarity.pdf"))
+    
+    ## Save the various model forms
+    saveRDS(tidy(model), file = here(output_path_model,"tidy_model.rds"))
+    saveRDS(augment(model), file=here(output_path_model,"augment_model.rds") )
+    saveRDS(glance(model), file=here(output_path_model,"glance_model.rds") )
+    
+    ## Print completion message 
+    print("Linear regression run successfully!")
   }
 }
-linear_regression()
+linear_regression(clean_data_path = opt$clean_data_path, 
+                  output_path_image = opt$output_path_image, 
+                  output_path_model = opt$output_path_model)
 
